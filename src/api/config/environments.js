335a3f2abf6da94e84bd6d2ba399c21ada7d136f
sozarg <mateoterr@gmail.com>
2025-12-01 22:50:59 -0300

    añado sec reto

--- src/api/config/environments.js
+++ src/api/config/environments.js
@@ -9,5 +9,6 @@ export default {
         name: process.env.DB_NAME,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD
-    }
+    },
+    session_key: process.env.SESSION_KEY
 };
